77372a24ec38c3832d2ed001eb832a9061a3eb61
teapot <grncbg@gmail.com>
2017-05-02 10:28:10 +0900

    UPDATE
    送信内容を変数から取得するよう変更

--- bot_post.py
+++ bot_post.py
@@ -4,11 +4,18 @@
 from slacker import Slacker
 import slackbot_settings
 
+CHANNEL = "bot-test"
+MESSAGE = "message"
+USERNAME = "user"
+ICON_EMOJI = ":smile:"
+AS_USER = False
+
 SLACK = Slacker(slackbot_settings.API_TOKEN)
 SLACK.chat.post_message(
-    "channel",
-    "message",
-    username="username",
-    icon_emoji=":emoji:",
+    CHANNEL,
+    MESSAGE,
+    username=USERNAME,
+    icon_emoji=ICON_EMOJI,
+    as_user=AS_USER,
     link_names=True
     )
