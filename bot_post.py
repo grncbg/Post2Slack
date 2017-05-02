da9bcc57c1615378543ac9359f23748bee2b3a59
teapot <grncbg@gmail.com>
2017-05-02 11:08:10 +0900

    UPDATE

--- bot_post.py
+++ bot_post.py
@@ -1,14 +1,26 @@
 # coding: utf-8
 """ botからの送信 """
 
+import sys
 from slacker import Slacker
 import slackbot_settings
 
-CHANNEL = "bot-test"
-MESSAGE = "message"
-USERNAME = "user"
-ICON_EMOJI = ":smile:"
-AS_USER = False
+ARGVS = sys.argv
+ARGC = len(ARGVS)
+
+if ARGC < 3:
+    print("too few arguments")
+    sys.exit()
+
+CHANNEL = ARGVS[1]
+MESSAGE = ARGVS[2]
+
+USERNAME = "金 正恩"
+ICON_EMOJI = ":kimjongun:"
+
+if ARGC == 5:
+    USERNAME = ARGVS[3]
+    ICON_EMOJI = ":" + ARGVS[4] + ":"
 
 SLACK = Slacker(slackbot_settings.API_TOKEN)
 SLACK.chat.post_message(
@@ -16,6 +28,5 @@ SLACK.chat.post_message(
     MESSAGE,
     username=USERNAME,
     icon_emoji=ICON_EMOJI,
-    as_user=AS_USER,
     link_names=True
     )
